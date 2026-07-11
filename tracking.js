828031d08e480d9a69596a1770662f70bb382ce7
skkr09 <skkrforgo@gmail.com>
2026-07-11 12:44:09 +0530

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -5,7 +5,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NTRJ5KHX');`;
+    })(window,document,'script','dataLayer','GTM-57G6KVXN');`;
     document.head.appendChild(gtmScript);
 })();
 
@@ -29,7 +29,7 @@
 // 2️⃣ Insert Google Tag Manager noscript inside <body>
 document.addEventListener("DOMContentLoaded", function() {
     var gtmNoScript = document.createElement("noscript");
-    gtmNoScript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTRJ5KHX"
+    gtmNoScript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57G6KVXN"
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
     document.body.insertBefore(gtmNoScript, document.body.firstChild);
 });
